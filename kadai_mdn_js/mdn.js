80b6c8a43a12df3ac55197effbadeb419e12274e
RISA333 <rira3y@gmail.com>
2025-01-31 15:06:43 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+const today = new Date(2024,10,12);
+const year = today.getFullYear();
+
+const month = today.getMonth();
+
+const day = today.getDate();
+
+    console.log(year + '年' + month + '月' + day + '日');
